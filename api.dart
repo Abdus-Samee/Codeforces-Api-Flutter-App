29d9bee7c45657dca2ead8666f21b0345b469e1b
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2020-10-24 16:58:25 +0600

    Added http request for rating changes

--- api.dart
+++ api.dart
@@ -5,14 +5,18 @@ import 'package:http/http.dart';
 class Api{
   var m;
   var arr;
+  var verdict;
+  var rating;
 
   Api(){
     m = new Map<String, int>();
+    verdict = new Map<String, int>();
+    rating = new Map<int, int>();
   }
 
   Future<void> getInfo() async {
     try{
-      Response res = await get('https://codeforces.com/api/user.status?handle=Fefer_Ivan&from=1&count=10');
+      Response res = await get('https://codeforces.com/api/user.status?handle=gamsahabnida&from=1&count=10');
       Map data = jsonDecode(res.body);
       arr = data['result'];
 
@@ -27,7 +31,22 @@ class Api{
         }
       }
 
+      for(int i = 0; i < arr.length; i++){
+        if(verdict.containsKey(arr[i]['verdict'])) verdict[arr[i]['verdict']] += 1;
+        else verdict[arr[i]['verdict']] = 1;
+      }
+
       print(m);
+      print(verdict);
+
+      Response res_rat = await get('https://codeforces.com/api/user.rating?handle=gamsahabnida');
+      Map data_rat = jsonDecode(res_rat.body);
+      arr = data_rat['result'];
+
+      for(int i = 0; i < arr.length; i++){
+        rating[i] = arr[i]['newRating'] ;
+      }
+
     }catch(e){
       print('Caught error: $e');
     }
